0def378b60ee78057ff6017e9b8f96f992aafdb8
Pablo <UO258082@uniovi.es>
2019-03-24 20:29:49 +0100

    SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
             respuesta += 'Autor: ' + req.query.autor;
         res.send(respuesta);
     });
+
     app.get('/suma', function(req, res) {
         var respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
@@ -16,9 +17,16 @@ module.exports = function(app) {
         var respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
+
     app.get('/canciones/:genero/:id', function(req, res) {
         var respuesta = 'id: ' + req.params.id + '<br>'
             + 'Genero: ' + req.params.genero;
         res.send(respuesta);
+    });
+
+    app.post("/cancion", function(req, res) {
+        res.send("Canción agregada:"+req.body.nombre +"<br>"
+            +" genero :" +req.body.genero +"<br>"
+            +" precio: "+req.body.precio);
     });
 };
