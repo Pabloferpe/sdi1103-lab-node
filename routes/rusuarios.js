7e103aca664f6c298bb9de4ba2da34aaf1f70125
Pablo <UO258082@uniovi.es>
2019-03-24 22:19:14 +0100

    SDINodeJS-8.9 y 8.10 - Colecciones relacionadas y routers

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -53,6 +53,67 @@ module.exports = function(app, swig, gestorBD) {
         res.send("Usuario desconectado");
     })
 
+    app.get("/publicaciones", function(req, res) {
+        var criterio = { autor : req.session.usuario };
+        gestorBD.obtenerCanciones(criterio, function(canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                var respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        canciones : canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/cancion/modificar/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                var respuesta = swig.renderFile('views/bcancionModificar.html',
+                    {
+                        cancion : canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
+
+    function paso1ModificarPortada(files, id, callback){
+        if (files.portada != null) {
+            var imagen =files.portada;
+            imagen.mv('public/portadas/' + id + '.png', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    paso2ModificarAudio(files, id, callback); // SIGUIENTE
+                }
+            });
+        } else {
+            paso2ModificarAudio(files, id, callback); // SIGUIENTE
+        }
+    };
+    function paso2ModificarAudio(files, id, callback){
+        if (files.audio != null) {
+            var audio = files.audio;
+            audio.mv('public/audios/'+id+'.mp3', function(err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    callback(true); // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    };
+
+
+
 
 
 };
