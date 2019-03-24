1e0e5ed3dcf7a6d2ad1f12d56b1578f3f4349aa3
Pablo <UO258082@uniovi.es>
2019-03-24 22:14:01 +0100

    SDI-NodeJS-8.5,67- Registro de usuario, autenticación y uso de sesión

--- app.js
+++ app.js
@@ -2,24 +2,32 @@
 var express = require('express');
 var app = express();
 
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+var crypto = require('crypto');
 var fileUpload = require('express-fileupload');
 app.use(fileUpload());
 var mongo = require('mongodb');
 var swig = require('swig');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
-
+gestorBD.init(app, mongo);
 
 
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
-
+app.set('db', 'mongodb://localhost:27017/uomusic');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 
 //Rutas/controladores por lógica
@@ -28,6 +36,6 @@ require("./routes/rcanciones.js")(app, swig, gestorBD);
 
 
 // lanzar el servidor
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log("Servidor activo");
 })
